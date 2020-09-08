fa89c0c7578ca605c1249b8e01d3aab156a6ba97
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-08 23:01:44 +0200

    array made

--- traversy-media-crash-course/main.js
+++ traversy-media-crash-course/main.js
@@ -1,11 +1,10 @@
-console.log('Hello World');
 console.error('This is an error'); //inserting an error message
 console.warn('This is a warning'); //inserting a warning
 
 /* Variables: There are 3 types of variables in js and they are the following.
-    var - this is the old method of using variables
-    let - this type of variable can be initialised with no given value and can be changed later on
-    const - this type of variable can neither be initialised with no value nor changed later on. 
+    var     - this is the old method of using variables
+    let     - this type of variable can be initialised with no given value and can be changed later on
+    const   - this type of variable can neither be initialised with no value nor changed later on. 
             In other words it is CONSTANT. 
             Always use const unless you intend on changing the variable later on
 */
@@ -23,3 +22,16 @@ console.log(typeof isCool); //typeof is used to see what type of variable it is
 //Concatenation vs template strings
 console.log('My name is ' + name + ' and i am ' + age); //concatenation
 console.log(`My name is ${name} and i am ${age}`); // template string
+
+//Arrays - arrays are just variables that hold multiple values
+const fruits = ['apples', 'oranges', 'bananas', 'pineapple']; // you can also put other data types in the array
+
+console.log(fruits); //viewing all values
+console.log(fruits[0]); // viewing an array value from a specific point
+
+// Adding values to an array
+fruits[2] = 'strawberry';   // adding at a specific point
+fruits.push('grapes');      // adding at the end of the array
+fruits.unshift('mangoe');   // adding at the beginning of the array
+
+console.log(fruits);
