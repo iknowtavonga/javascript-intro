c6f22bbb38dfd55835b77e3684362e6e997e3e34
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-09 22:28:14 +0200

    basic addition multiplator complete

---
+++ Basic-calculator/main.js
@@ -0,0 +1,9 @@
+var num1 = window.prompt('Enter a number');
+var num2 = window.prompt('Enter another number');
+
+// converting the input from string to float
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+// adding the numbers together
+document.write(num1 + num2);
