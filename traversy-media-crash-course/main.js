4996dc36a03f000cc72305eb4024b2886ebb3716
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-10 20:42:04 +0200

    traversy array tutorial complete

--- traversy-media-crash-course/main.js
+++ traversy-media-crash-course/main.js
@@ -17,22 +17,28 @@ const x = null;
 const y = undefined;
 let z; // This is another way of making an undefined variable
 
-console.log(name[name.length - 2]);
+//position of a character to the nth number
+console.log(name[name.length - 2]); // returns 'h' because it is second from last
+
 console.log(typeof isCool); //typeof is used to see what type of variable it is
 
 //Concatenation vs template strings
 console.log('My name is ' + name + ' and i am ' + age); //concatenation
 console.log(`My name is ${name} and i am ${age}`); // template string
 
-//Arrays - arrays are just variables that hold multiple values
+
+//CREATING AN ARRAY - arrays are just variables that hold multiple values
 const fruits = ['apples', 'oranges', 'bananas', 'pineapple']; // you can also put other data types in the array
 
 console.log(fruits); //viewing all values
 console.log(fruits[0]); // viewing an array value from a specific point
 
 // Adding values to an array
+console.log(Array.isArray(fruits)); // checking to see if something is an array
 fruits[2] = 'strawberry';   // adding at a specific point
+fruits[fruits.length - 3] = 'pear'; // adding at forth position from last
 fruits.push('grapes');      // adding at the end of the array
 fruits.unshift('mangoe');   // adding at the beginning of the array
 
 console.log(fruits);
+console.log(fruits.indexOf('pear'));
