2905ad36d211bbee77b9d2f10488d8a13d80dab6
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-11 21:57:23 +0200

    creating an object within an array

--- traversy-media-crash-course/objects.js
+++ traversy-media-crash-course/objects.js
@@ -15,3 +15,33 @@ const person = {
 }
 
 console.log(person);    // displaying the whole object
+console.log(person.name, person.surname);    // accessing specific items from the object
+console.log(person.hobbies[1]);    // accessing a specific value in an array in the object
+console.log(person.address);    //accessing an object within an object
+console.log(person.address.city);   //accessing a nested object's value
+
+person.email = 'johnd@gmail.com'   //directly adding properties to the object
+console.log(person);
+
+//Creating an object within an array
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true 
+    },
+    // Item 2 of array below
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true 
+    },
+    // Item 3 of array
+    {
+        id: 3,
+        text: 'Dentist appointment',
+        isCompleted: false
+    },
+];
+
+console.log(todos[1].text); // targeting a specific object property within your array
