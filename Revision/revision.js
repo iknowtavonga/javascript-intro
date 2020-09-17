f659315283d12b60b569ffcc30644db5328b233b
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-17 21:08:48 +0200

    started a javascript revision exercise

---
+++ Revision/revision.js
@@ -0,0 +1,12 @@
+// variables 
+let firstName = 'Tavonga';
+let surname = 'Karimanzira';
+let age = 20;
+let isMale = true;
+let siblings = 3;
+let spouse = null;
+
+const human = [firstName, surname, age];
+console.log(human);
+
+console.log(`My name is ${firstName} ${surname}. I am ${age} years old.`)
