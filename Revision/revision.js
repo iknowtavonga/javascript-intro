9d4f971d9e1b0d86f43ed98b4e5165a4f5abe2b1
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-17 21:34:34 +0200

    started array revision

--- Revision/revision.js
+++ Revision/revision.js
@@ -6,7 +6,8 @@ let isMale = true;
 let siblings = 3;
 let spouse = null;
 
+console.log(`My name is ${firstName} ${surname}. I am ${age} years old.\nI have ${siblings} siblings.`);
+
+// Arrays
 const human = [firstName, surname, age];
 console.log(human);
-
-console.log(`My name is ${firstName} ${surname}. I am ${age} years old.`)
