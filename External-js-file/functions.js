e1d53b5b47436b4ee636b442bd30d30efca968c2
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-21 21:41:24 +0200

    completed pure function declaration

--- External-js-file/functions.js
+++ External-js-file/functions.js
@@ -1,5 +1,12 @@
 // These are the ways you can call functions
 
+// METHOD 1 - Pure function declaration
+function greeting(){
+    console.log('I am your guide, how can i assist you?');
+}
+
+greeting();
+
 // METHOD 1 - Immediately Invoked Function Expression
 (function welcomeMessage(){
     console.log('Hey there! Thank you for visiting');
