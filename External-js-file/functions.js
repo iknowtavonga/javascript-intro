5a4bd6459f7e216300c3da5f527742b6494cf8b0
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-21 21:32:23 +0200

    created an Imediately Invoked Function Expression

---
+++ External-js-file/functions.js
@@ -0,0 +1,7 @@
+// These are the ways you can call functions
+
+// METHOD 1 - Immediately Invoked Function Expression
+(function welcomeMessage(){
+    console.log('Hey there! Thank you for visiting');
+})();
+
