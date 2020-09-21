574d87fa90444bfdd7e016cd7b9e30106f6997b7
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-21 22:08:42 +0200

    completed helper functions

--- External-js-file/functions.js
+++ External-js-file/functions.js
@@ -6,15 +6,34 @@ function greeting(){
 }
 greeting();
 
-
-// METHOD 2 - Variable declaration function
+// METHOD 2 - Variable declaration function / ANNONYMOUS FUNCTION
 const personalGuide = function(name){
     console.log(`Hi there! My name is ${name} and i will be your guide today.`);
 }
 ('Tavonga');
 
-// METHOD 1 - Immediately Invoked Function Expression
+// METHOD 3 - Immediately Invoked Function Expression
 (function welcomeMessage(){
     console.log('Hey there! Thank you for visiting');
 })();
 
+// HELPER FUNCTIONS
+/*  Helper functions are  functions being called within another function.
+    Since each function is carrying out a specific task, it makes our code easier to read and debug if necessary.
+*/
+
+// first we declare the function that'll get the number of monitors we need
+function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  
+  // next we declare a function that multiplies the monitors we need with the price of 1 monitor
+  
+  function costOfMonitors(rows, columns){
+    return monitorCount(rows, columns) * 200;
+  }
+  
+  // we then store the cost in a variable that takes in the dimension of the space needed
+  
+  const totalCost = costOfMonitors(5, 4);
+  console.log(totalCost);
