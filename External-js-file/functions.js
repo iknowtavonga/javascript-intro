87e5557283d31f667e3b0798dabf1d26c9305d8e
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-21 22:04:25 +0200

    completed variable declaration functions

--- External-js-file/functions.js
+++ External-js-file/functions.js
@@ -4,8 +4,14 @@
 function greeting(){
     console.log('I am your guide, how can i assist you?');
 }
-
 greeting();
+
+
+// METHOD 2 - Variable declaration function
+const personalGuide = function(name){
+    console.log(`Hi there! My name is ${name} and i will be your guide today.`);
+}
+('Tavonga');
 
 // METHOD 1 - Immediately Invoked Function Expression
 (function welcomeMessage(){
