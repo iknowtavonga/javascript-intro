c220d6da6354029a13d45b845eb9b5d48cd2ba14
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-28 22:16:17 +0200

    completed functions

--- clever-programmer-8-hours/main.js
+++ clever-programmer-8-hours/main.js
@@ -1,5 +1,29 @@
 var b = "smoothie";
 console.log(b);
 
-// Grabbing html elements
-document.getElementById('some-text').innerHTML = 'Hey buddy' + ', want a ' + b + '?';
+// Prompts and storing prompts
+/* 
+
+var name = prompt('What is your name?');
+console.log(name);
+
+// Manipulating the DOM
+document.getElementById('some-text').innerHTML = `Hey ${name}, want a ${b}?`;
+
+*/
+
+// Incrementing numbers
+var age = 23;
+console.log(age);
+
+age += 2;
+console.log(age);
+
+// Creating functions
+function greeting(yourName){
+    var result = document.getElementById('some-text').innerHTML = `Hey there ${yourName}, nice to meet you`;
+    return result;
+}
+
+//var name = prompt('What is your name?');
+//greeting(name);
